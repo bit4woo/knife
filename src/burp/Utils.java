6ae501cc88881413bb1ff94deb0f8c2169ecad1f
bit4woo <bit4woo@163.com>
2019-07-09 21:19:56 +0800

    plan to use a class for cookie to set

--- src/burp/Utils.java
+++ src/burp/Utils.java
@@ -1,5 +1,9 @@
 package burp;
 
+import java.awt.Desktop;
+import java.net.URI;
+import java.net.URL;
+
 public class Utils {
     public static boolean isWindows() {
     	String OS_NAME = System.getProperties().getProperty("os.name").toLowerCase();
@@ -9,4 +13,29 @@ public class Utils {
         	return false;
         }
     }
+    
+    
+    public static void browserOpen(Object url,String browser) throws Exception{
+		String urlString = null;
+		URI uri = null;
+		if (url instanceof String) {
+			urlString = (String) url;
+			uri = new URI((String)url);
+		}else if (url instanceof URL) {
+			uri = ((URL)url).toURI();
+			urlString = url.toString();
+		}
+		if(browser == null ||browser.equalsIgnoreCase("default") || browser.equalsIgnoreCase("")) {
+			//whether null must be the first
+			Desktop desktop = Desktop.getDesktop();
+			if(Desktop.isDesktopSupported()&&desktop.isSupported(Desktop.Action.BROWSE)){
+				desktop.browse(uri);
+			}
+		}else {
+			Runtime runtime = Runtime.getRuntime();
+			runtime.exec(browser+" "+urlString);
+			//C:\Program Files\Mozilla Firefox\firefox.exe
+			//C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe
+		}
+	}
 }
---
+++ src/knife/Cookie.java
@@ -0,0 +1,11 @@
+package knife;
+
+import java.util.HashSet;
+
+public class Cookie {
+	String cookieSource;
+	String cookieValue;
+	HashSet<String> targetUrls;
+	String birthtime;
+	//TODO
+}
