fa26e5f9ca54dd3fb52c598452b0b30328f7e436
bit4woo <bit4woo@163.com>
2019-08-04 23:25:34 +0800

    insert payload to json parameter ---in dev

--- src/knife/InsertXSSMenu.java
+++ src/knife/InsertXSSMenu.java
@@ -1,19 +1,16 @@
 package knife;
 
+import burp.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.PrintWriter;
+import java.util.Iterator;
 import java.util.List;
-
-import javax.swing.JMenuItem;
-
-import burp.BurpExtender;
-import burp.Getter;
-import burp.IBurpExtenderCallbacks;
-import burp.IContextMenuInvocation;
-import burp.IExtensionHelpers;
-import burp.IHttpRequestResponse;
-import burp.IParameter;
 
 public class InsertXSSMenu extends JMenuItem {
 	//JMenuItem vs. JMenu
@@ -44,31 +41,45 @@ class InsertXSSAction implements ActionListener {
 	public void actionPerformed(ActionEvent event) {
 
 		IHttpRequestResponse[] selectedItems = invocation.getSelectedMessages();
-		
+
 		IHttpRequestResponse messageInfo = selectedItems[0];
 		byte[] newRequest = messageInfo.getRequest();
-		
+
 		Getter getter = new Getter(helpers);
 		List<IParameter> paras = getter.getParas(messageInfo);
 		String xsspayload = burp.tableModel.getConfigByKey("XSS-Payload");
-		
+
 		if (xsspayload == null) return;
-		
+
 		for(IParameter para:paras) {
-			String value = para.getValue();
-			if (para.getType() != IParameter.PARAM_COOKIE) {
-				if (isInt(value)) {
-					continue;
-				}else {
-					value = value+xsspayload;
-					IParameter newPara = helpers.buildParameter(para.getName(), value, para.getType());
-					newRequest = helpers.updateParameter(newRequest, newPara);
-				}
+			if (para.getType() == IParameter.PARAM_COOKIE) {
+				continue;
 			}
+
+			String value = para.getValue();
+
+			if (isInt(value)) {
+				continue;
+			}
+
+			if (para.getType() == IParameter.PARAM_JSON) {
+				System.out.println(para.getValue());
+			}
+
+			if (para.getType() == IParameter.PARAM_URL) {
+				value = value + helpers.urlEncode(xsspayload);
+			}else{
+				value = value+xsspayload;
+			}
+
+
+
+			IParameter newPara = helpers.buildParameter(para.getName(), value, para.getType());
+			newRequest = helpers.updateParameter(newRequest, newPara);
 		}
 		messageInfo.setRequest(newRequest);
 	}
-	
+
 	public static boolean isInt(String input) {
 		try {
 			Integer b = Integer.valueOf(input);
@@ -78,13 +89,59 @@ class InsertXSSAction implements ActionListener {
 				long l = Long.valueOf(input);
 				return true;
 			}catch(Exception e1) {
-				
+
 			}
 			return false;
 		}
 	}
-	
+
+	public static JSONObject updateJSONValue(JSONObject obj, String newValue) throws Exception {
+		// We need to know keys of Jsonobject
+		Iterator iterator = obj.keys();
+		while (iterator.hasNext()) {
+			String key = (String) iterator.next();
+
+			// if object is just string we change value in key
+			if ((obj.optJSONArray(key)==null) && (obj.optJSONObject(key)==null)) {
+				// put new value
+				String value = (String) obj.get(key);
+				obj.put(key, value+newValue);
+			}
+
+			// if it's jsonobject
+			if (obj.optJSONObject(key) != null) {
+				updateJSONValue(obj.getJSONObject(key), newValue);
+			}
+
+			// if it's jsonarray
+			if (obj.optJSONArray(key) != null) {
+				JSONArray jArray = obj.getJSONArray(key);
+				JSONArray jArrayResult =new JSONArray();
+				for (int i=0;i<=jArray.length();i++) {
+					JSONObject newJSONObject = updateJSONValue(jArray.getJSONObject(i), newValue);
+					jArrayResult.put(newJSONObject);
+				}
+				obj.put(key, jArrayResult);
+			}
+		}
+		return obj;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(isInt("13175192849"));
+		//System.out.println(isInt("13175192849"));
+		String aaa = "{\r\n" + 
+				" \"person\":{\"name\":\"Sam\", \"surname\":\"ngonma\"},\r\n" + 
+				" \"car\":{\"make\":\"toyota\", \"model\":\"yaris\"}\r\n" + 
+				" }";
+		new JSONObject(aaa);
+		try {
+			System.out.println(updateJSONValue(new JSONObject(aaa),"11111"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
