befcd6da02e2d5a4e33274ce451a9dcd31285360
bit4woo <bit4woo@163.com>
2020-05-20 11:38:07 +0800

    更新OpenWithBrowse功能
    在包含了中文的页面中也能正确选中URL了，前提是burp设置了正确的显示编码！
    优化了URL的拼接逻辑。

--- src/burp/Utils.java
+++ src/burp/Utils.java
@@ -3,40 +3,47 @@ package burp;
 import java.awt.Desktop;
 import java.net.URI;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.ibm.icu.text.CharsetDetector;
+import com.ibm.icu.text.CharsetMatch;
 
 public class Utils {
-    public static boolean isWindows() {
-    	String OS_NAME = System.getProperties().getProperty("os.name").toLowerCase();
-    	//System.out.println(OS_NAME);
-        if (OS_NAME.contains("windows")) {
-            return true;
-        } else {
-        	return false;
-        }
-    }
-    
-    public static boolean isWindows10() {
-    	String OS_NAME = System.getProperties().getProperty("os.name").toLowerCase();
-    	if (OS_NAME.equalsIgnoreCase("windows 10")) {
-    		return true;
-    	}
-    	return false;
-    }
-    
+	public static boolean isWindows() {
+		String OS_NAME = System.getProperties().getProperty("os.name").toLowerCase();
+		//System.out.println(OS_NAME);
+		if (OS_NAME.contains("windows")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean isWindows10() {
+		String OS_NAME = System.getProperties().getProperty("os.name").toLowerCase();
+		if (OS_NAME.equalsIgnoreCase("windows 10")) {
+			return true;
+		}
+		return false;
+	}
+
 	public static boolean isMac(){
 		String os = System.getProperty("os.name").toLowerCase();
 		//Mac
-	    return (os.indexOf( "mac" ) >= 0); 
+		return (os.indexOf( "mac" ) >= 0); 
 	}
- 
+
 	public static boolean isUnix(){
 		String os = System.getProperty("os.name").toLowerCase();
 		//linux or unix
-	    return (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0);
+		return (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0);
 	}
-    
-    
-    public static void browserOpen(Object url,String browser) throws Exception{
+
+
+	public static void browserOpen(Object url,String browser) throws Exception{
 		String urlString = null;
 		URI uri = null;
 		if (url instanceof String) {
@@ -58,5 +65,51 @@ public class Utils {
 			//C:\Program Files\Mozilla Firefox\firefox.exe
 			//C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe
 		}
+	}
+
+	public static String getResponseCharset(byte[] response){
+		Getter getter = new Getter(BurpExtender.callbacks.getHelpers());
+		String contentType = getter.getHeaderValueOf(false,response,"Content-Type");
+		String body = new String(getter.getBody(false,response));
+		String tmpcharSet = null;
+
+		if (contentType != null){//1、尝试从contentTpye中获取
+			if (contentType.toLowerCase().contains("charset=")) {
+				tmpcharSet = contentType.toLowerCase().split("charset=")[1];
+			}
+		}
+
+		if (tmpcharSet == null){//2、尝试从body中获取
+			Pattern pDomainNameOnly = Pattern.compile("charset=(.*?)>");
+			Matcher matcher = pDomainNameOnly.matcher(body);
+			if (matcher.find()) {
+				tmpcharSet = matcher.group(0).toLowerCase();
+				//				tmpcharSet = tmpcharSet.replace("\"","");
+				//				tmpcharSet = tmpcharSet.replace(">","");
+				//				tmpcharSet = tmpcharSet.replace("/","");
+				//				tmpcharSet = tmpcharSet.replace("charset=","");
+			}
+		}
+
+		if (tmpcharSet == null){//3、尝试使用ICU4J进行编码的检测
+			CharsetDetector detector = new CharsetDetector();
+			detector.setText(response);
+			CharsetMatch cm = detector.detect();
+			tmpcharSet = cm.getName();
+		}
+
+		tmpcharSet = tmpcharSet.toLowerCase().trim();
+		if (tmpcharSet.contains("utf8")){
+			tmpcharSet = "utf-8";
+		}else {
+			//常见的编码格式有ASCII、ANSI、GBK、GB2312、UTF-8、GB18030和UNICODE等。
+			List<String> commonCharSet = Arrays.asList("ASCII,ANSI,GBK,GB2312,UTF-8,GB18030,UNICODE,ISO-8859-1".toLowerCase().split(","));
+			for (String item:commonCharSet) {
+				if (tmpcharSet.contains(item)) {
+					tmpcharSet = item;
+				}
+			}
+		}
+		return tmpcharSet;
 	}
 }
--- src/knife/OpenWithBrowserMenu.java
+++ src/knife/OpenWithBrowserMenu.java
@@ -14,7 +14,6 @@ import burp.IBurpExtenderCallbacks;
 import burp.IContextMenuInvocation;
 import burp.IExtensionHelpers;
 import burp.IHttpRequestResponse;
-import burp.RobotInput;
 import burp.Utils;
 
 public class OpenWithBrowserMenu extends JMenuItem {
@@ -67,11 +66,12 @@ class OpenWithBrowser_Action implements ActionListener{
 				//String selectedUrl = new RobotInput().getSelectedString();//为了解决burp API中的bug，尝试用复制粘贴方法获取选中的内容。
 				//不知道为什么这里获取到的结果始终是上一次复制的内容。
 				//难道是因为，当用鼠标点击右键菜单时，当前的选中内容不是burp数据表中的字符串，而是当前菜单项？所以这个方法走不通了？
+				//stderr.println("selected URL: "+selectedUrl);
+				//stderr.println("selected URL old: "+getSelectedStringByBurpOld());
 				
-				
-				
-				stderr.println("selected URL: "+selectedUrl);
 				if (selectedUrl.length()>10) {// http://a.cn
+					stdout.println();
+					stdout.println("//////////open URL: "+selectedUrl+" //////////");
 					Utils.browserOpen(selectedUrl,browserPath);
 					//stdout.println(selectedUrl);
 				}else {
@@ -87,6 +87,9 @@ class OpenWithBrowser_Action implements ActionListener{
 			}else {
 				stderr.println("Please Select Less URLs to Open");
 			}
+		}
+		catch (java.net.URISyntaxException e) {
+			stderr.println(e.getMessage());
 		}
 		catch (Exception e1)
 		{
@@ -106,28 +109,52 @@ class OpenWithBrowser_Action implements ActionListener{
 		return num;
 	}
 	
-	public int isChinese(String a)  {
-		char[] c = a.toCharArray();
-		int count=0;
-		for (char d : c) {
-			Character.UnicodeBlock ub = Character.UnicodeBlock.of(d);
-			if (ub == Character.UnicodeBlock.GENERAL_PUNCTUATION
-					|| ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
-					|| ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
-					|| ub == Character.UnicodeBlock.CJK_COMPATIBILITY_FORMS
-					|| ub == Character.UnicodeBlock.VERTICAL_FORMS
-					|| ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
-					|| ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
-					|| ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B
-					|| ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_C
-					|| ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_D
-					|| ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
-					|| ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS_SUPPLEMENT) {
-				   count++;
-			} 
- 
+	@Deprecated
+	public String getSelectedStringByBurpOld(){
+		String result = "";
+
+		IHttpRequestResponse[] messages = invocation.getSelectedMessages();
+
+		if (messages == null ) {
+			return result;
 		}
-		 return count;
+
+		if (messages.length == 1) {
+			IHttpRequestResponse message = messages[0];
+			/////////////selected url/////////////////
+			byte[] source = null;
+
+
+			int context = invocation.getInvocationContext();
+			if (context==IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_REQUEST
+					|| context ==IContextMenuInvocation.CONTEXT_MESSAGE_VIEWER_REQUEST
+					|| context == IContextMenuInvocation.CONTEXT_PROXY_HISTORY
+					|| context == IContextMenuInvocation.CONTEXT_INTRUDER_ATTACK_RESULTS
+					|| context == IContextMenuInvocation.CONTEXT_SEARCH_RESULTS
+					|| context == IContextMenuInvocation.CONTEXT_TARGET_SITE_MAP_TABLE
+					|| context == IContextMenuInvocation.CONTEXT_TARGET_SITE_MAP_TREE) {
+				source = message.getRequest();
+			}else {
+				source = message.getResponse();
+			}
+
+			int[] selectedIndex = invocation.getSelectionBounds();//当数据包中有中文或其他宽字符的时候，这里的返回值不正确。已报bug。
+			//获得的index根据选中内容前面中文字数的个数*2 的值前移了。
+			//stdout.println(selectedIndex[0]+":"+selectedIndex[1]);
+
+			if(source!=null && selectedIndex !=null && selectedIndex[1]-selectedIndex[0]>=3) {
+				int selectedLength = selectedIndex[1]-selectedIndex[0];
+				byte[] selectedBytes = new byte[selectedLength];
+				System.arraycopy(source, selectedIndex[0], selectedBytes, 0, selectedLength);//新的内容替换选中内容
+				stderr.println("11--->"+burp.callbacks.getHelpers().bytesToString(selectedBytes));
+				stderr.println("22--->"+ new String(selectedBytes));
+				result = new String(selectedBytes).trim();
+				
+			}
+
+			result = getFullUrl(result,message);
+		}
+		return result;
 	}
 
 	public String getSelectedStringByBurp(){
@@ -157,40 +184,56 @@ class OpenWithBrowser_Action implements ActionListener{
 			}else {
 				source = message.getResponse();
 			}
-			
-			stderr.println("chinese count: "+isChinese(new String(source)));
 
 			int[] selectedIndex = invocation.getSelectionBounds();//当数据包中有中文或其他宽字符的时候，这里的返回值不正确。已报bug。
-			//获得的index根据选中内容前面中文字数的个数*2 的值前移了。
 			//stdout.println(selectedIndex[0]+":"+selectedIndex[1]);
+			//这里的index应该是字符串的index，进行选中操作时对象应该是字符文本内容，无论是一个中文还是一个字母，都是一个文本字符。这就是我们通常的文本操作啊，之前是想多了。
+			//burp进行的byte和string之间的转换，没有考虑特定的编码，是一刀切的方式，所以将index用于byte序列上，就不能正确对应。
 
 			if(source!=null && selectedIndex !=null && selectedIndex[1]-selectedIndex[0]>=3) {
-				int selectedLength = selectedIndex[1]-selectedIndex[0];
-				byte[] selectedBytes = new byte[selectedLength];
-				System.arraycopy(source, selectedIndex[0], selectedBytes, 0, selectedLength);//新的内容替换选中内容
-				stderr.println("11--->"+burp.callbacks.getHelpers().bytesToString(selectedBytes));
-				stderr.println("22--->"+ new String(selectedBytes));
-				result = new String(selectedBytes).trim();
-				
-			}
-
-			if(!isFullUrl(result)) {
-				result = message.getHttpService().toString()+"/"+result;
-			}else if(result.startsWith("//")) {
-				result = message.getHttpService().getProtocol()+":"+result;
+				String originalCharSet = Utils.getResponseCharset(source);
+				String text;
+				try {
+					text = new String(source,originalCharSet);
+				}catch(Exception e) {
+					text = new String(source);
+				}
+				result = text.substring(selectedIndex[0], selectedIndex[1]);
+				result = getFullUrl(result,message);
 			}
 		}
 		return result;
 	}
+	
+	//<script src="http://lbs.sf-express.com/api/map?v=2.0&ak=b1cfb18ca6864e46b3ed4cb18f12c0f8">
+	//<script type=text/javascript src=./static/js/manifest.c7ad14f4845199970dcb.js>
+	//<link rel="stylesheet" type="text/css" href="/cat/assets/css/bootstrap.min.css">
+	//<link href=static/css/chunk-03d2ee16.a3503987.css rel=prefetch>
+	//<link href="www.microsoft.com">这只会被当成目标，不会被当成域名
+	//<link href="//www.microsoft.com">会被当成域名，协议会使用当前页面所使用的协议
+	public static String getFullUrl(String url,IHttpRequestResponse message) {
+		if (url.startsWith("http://") || url.startsWith("https://")) {
+			//都是带有host的完整URL，直接访问即可
+			return url;
+			
+		}else if(url.startsWith("//")) {//使用当前web的请求协议
+			
+			return message.getHttpService().getProtocol()+":"+url;
 
-
-	public static boolean isFullUrl(String url) {
-		if (url.startsWith("//") || url.startsWith("http://") || url.startsWith("https://")) {//    //misc.360buyimg.com/
-			return true;
-		}else if (url.startsWith("../") || url.startsWith("./") || url.startsWith("/")) {
-			return false;
-		}else {
-			return true;
+		}else if (url.startsWith("../") || url.startsWith("./") ) {
+			
+			return message.getHttpService().toString()+"/"+url;
+			
+		}else if(url.startsWith("/")){
+			
+			return message.getHttpService().toString()+url;
+			
+		}else{//没有斜杠的情况。<link href="www.microsoft.com">这只会被当成目标，不会被当成域名
+			
+			Getter getter = new Getter(BurpExtender.callbacks.getHelpers());
+			String fullUrl = getter.getFullURL(message).toString().split("\\?")[0];
+			int indexOfLastSlash = fullUrl.lastIndexOf("/");//截取的内容不不包含当前index对应的元素
+			return fullUrl.substring(0,indexOfLastSlash+1)+url;
 		}
 	}
 }
