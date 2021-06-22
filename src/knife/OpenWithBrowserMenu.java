f433f26fbc7ead5ed9b6163085f5884ca9c2cf13
bit4woo <bit4woo@163.com>
2021-06-22 15:13:56 +0800

    优化代码

--- src/knife/OpenWithBrowserMenu.java
+++ src/knife/OpenWithBrowserMenu.java
@@ -9,8 +9,8 @@ import java.net.URL;
 import javax.swing.JMenuItem;
 
 import burp.BurpExtender;
-import burp.CharSet;
 import burp.Getter;
+import burp.HttpMessageCharSet;
 import burp.IBurpExtenderCallbacks;
 import burp.IContextMenuInvocation;
 import burp.IExtensionHelpers;
@@ -192,7 +192,7 @@ class OpenWithBrowser_Action implements ActionListener{
 			//burp进行的byte和string之间的转换，没有考虑特定的编码，是一刀切的方式，所以将index用于byte序列上，就不能正确对应。
 
 			if(source!=null && selectedIndex !=null && selectedIndex[1]-selectedIndex[0]>=3) {
-				String originalCharSet = CharSet.getResponseCharset(source);
+				String originalCharSet = HttpMessageCharSet.getCharset(source);
 				String text;
 				try {
 					text = new String(source,originalCharSet);
