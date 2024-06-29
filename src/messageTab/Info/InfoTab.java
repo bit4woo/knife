f2294c2f57e6e6121c543cf712638a5d864f8962
bit4woo <bit4woo@163.com>
2024-06-29 11:12:57 +0800

    update

--- src/messageTab/Info/InfoTab.java
+++ src/messageTab/Info/InfoTab.java
@@ -24,153 +24,153 @@ import burp.IMessageEditorTab;
  * @github https://github.com/bit4woo
  */
 public class InfoTab implements IMessageEditorTab {
-    private JPanel panel;
-    private byte[] originContent;
-    public IMessageEditorController controller;
+	private JPanel panel;
+	private byte[] originContent;
+	public IMessageEditorController controller;
 
-    int triggerTime = 1;
-    boolean debug = false;
+	int triggerTime = 1;
+	boolean debug = false;
 
-    public byte[] getOriginContent() {
-        return originContent;
-    }
+	public byte[] getOriginContent() {
+		return originContent;
+	}
 
-    public void setOriginContent(byte[] originContent) {
-        this.originContent = originContent;
-    }
-
-
-    public IMessageEditorController getController() {
-        return controller;
-    }
-
-    public void setController(IMessageEditorController controller) {
-        this.controller = controller;
-    }
-
-    public InfoTab(IMessageEditorController controller, boolean editable, IExtensionHelpers helpers, IBurpExtenderCallbacks callbacks) {
-        this.controller = controller;
-        panel = new InfoPanel(this);
-        BurpExtender.getCallbacks().customizeUiComponent(panel);//尝试使用burp的font size
-    }
-
-    @Override
-    public String getTabCaption() {
-        return "Info";
-    }
-
-    @Override
-    public Component getUiComponent() {
-        return panel;
-    }
-
-    @Override
-    public boolean isEnabled(byte[] content, boolean isRequest) {
-        if (isRequest) {
-            return false;
-        }
-        String contentType = BurpExtender.getHelperPlus().getHeaderValueOf(isRequest, content, "Content-Type");
-        if (StringUtils.isEmpty(contentType)) {
-            return true;
-        }
-        if (contentType.contains("image/")) {
-            return false;
-        } else if (contentType.contains("text/css")) {
-            return false;
-        } else if (contentType.contains("font/")) {
-            return false;
-        } else if (contentType.contains("x-protobuf")) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * 每次切换到这个tab，都会调用这个函数。应考虑避免重复劳动，根据originContent是否变化来判断。
-     * 测试发现:
-     * 当在proxy页面点击一个数据包进行数据包切换时，触发infoTab处理逻辑，同一个数据包这个函数会被触发2次！
-     * <p>
-     * 不是请求、响应各调用一次；而是响应包被调用两次，请求包被调用两次。而且this是同一个对象。
-     * <p>
-     * 第一次触发，content内容不是当前数据包（点击选择的数据包）的，而是上一个数据包的。
-     * 第二次触发，content才是当前数据包的内容，也就是点击选择的数据包的内容。
-     * 造成一个结果就是，切换到新的数据包后，上一个数据包中提取到的内容会在当前数据包中显示。
-     */
-    @Override
-    public void setMessage(byte[] content, boolean isRequest) {
-        if (isRequest) {
-            return;
-        }
-
-//        boolean debug = true;
-        if (debug) {
-            System.out.println("\n\n##################");
-            System.out.println("triggerTime:" + triggerTime++);
-            System.out.println(controller.getHttpService());
-            System.out.println("content from controller:\n" + new String(controller.getResponse()));
-            System.out.println("content from parameter:\n" + new String(content));//切换数据包时，第一次的触发会发现这个内容是上一个数据包的。
-            System.out.println("equal:\n" + ByteArrayUtils.equals(controller.getResponse(), content));
-            System.out.println(this);
-            System.out.println(((InfoPanel) panel).getTable().getInfoTableModel());
-            System.out.println("##################");
-        }
-
-        content = controller.getResponse();
-        //从controller中获取真实的数据包，避免上面提到的，content是上一个数据包的问题。
-        if (content == null || content.length == 0) {
-            return;
-        } else if (ByteArrayUtils.equals(originContent, content)) {
-            return;
-        } else {
-            originContent = content;
-            SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
-                @Override
-                protected Void doInBackground() throws Exception {
-
-                    List<String> urls = FindUrlAction.findUrls(originContent);
-
-                    for (String url : urls) {
-                        InfoEntry aaa = new InfoEntry(url, InfoEntry.Type_URL);
-                        ((InfoPanel) panel).getTable().getInfoTableModel().addNewInfoEntry(aaa);
-                    }
-
-                    List<String> emails = EmailUtils.grepEmail(new String(originContent));
-                    emails = TextUtils.deduplicate(emails);
-                    for (String email : emails) {
-                        InfoEntry aaa = new InfoEntry(email, InfoEntry.Type_Email);
-                        ((InfoPanel) panel).getTable().getInfoTableModel().addNewInfoEntry(aaa);
-                    }
-
-                    return null;
-                }
-            };
-            worker.execute();
-        }
-    }
+	public void setOriginContent(byte[] originContent) {
+		this.originContent = originContent;
+	}
 
 
-    @Override
-    public byte[] getMessage() {
-        return originContent;
-    }
+	public IMessageEditorController getController() {
+		return controller;
+	}
 
-    @Override
-    public boolean isModified() {
-        return false;
-    }
+	public void setController(IMessageEditorController controller) {
+		this.controller = controller;
+	}
 
-    /**
-     * ctrl+c复制数据逻辑会调用这个函数
-     */
-    @Override
-    public byte[] getSelectedData() {
-        InfoTable table = (InfoTable) ((InfoPanel) panel).getTable();
-        String content = table.getSelectedContent();
-        return content.getBytes();
-    }
+	public InfoTab(IMessageEditorController controller, boolean editable, IExtensionHelpers helpers, IBurpExtenderCallbacks callbacks) {
+		this.controller = controller;
+		panel = new InfoPanel(this);
+		BurpExtender.getCallbacks().customizeUiComponent(panel);//尝试使用burp的font size
+	}
+
+	@Override
+	public String getTabCaption() {
+		return "Info";
+	}
+
+	@Override
+	public Component getUiComponent() {
+		return panel;
+	}
+
+	@Override
+	public boolean isEnabled(byte[] content, boolean isRequest) {
+		if (isRequest) {
+			return false;
+		}
+		String contentType = BurpExtender.getHelperPlus().getHeaderValueOf(isRequest, content, "Content-Type");
+		if (StringUtils.isEmpty(contentType)) {
+			return true;
+		}
+		if (contentType.contains("image/")) {
+			return false;
+		} else if (contentType.contains("text/css")) {
+			return false;
+		} else if (contentType.contains("font/")) {
+			return false;
+		} else if (contentType.contains("x-protobuf")) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * 每次切换到这个tab，都会调用这个函数。应考虑避免重复劳动，根据originContent是否变化来判断。
+	 * 测试发现:
+	 * 当在proxy页面点击一个数据包进行数据包切换时，触发infoTab处理逻辑，同一个数据包这个函数会被触发2次！
+	 * <p>
+	 * 不是请求、响应各调用一次；而是响应包被调用两次，请求包被调用两次。而且this是同一个对象。
+	 * <p>
+	 * 第一次触发，content内容不是当前数据包（点击选择的数据包）的，而是上一个数据包的。
+	 * 第二次触发，content才是当前数据包的内容，也就是点击选择的数据包的内容。
+	 * 造成一个结果就是，切换到新的数据包后，上一个数据包中提取到的内容会在当前数据包中显示。
+	 */
+	@Override
+	public void setMessage(byte[] content, boolean isRequest) {
+		if (isRequest) {
+			return;
+		}
+
+		//boolean debug = true;
+		if (debug) {
+			System.out.println("\n\n##################");
+			System.out.println("triggerTime:" + triggerTime++);
+			System.out.println(controller.getHttpService());
+			System.out.println("content from controller:\n" + new String(controller.getResponse()));
+			System.out.println("content from parameter:\n" + new String(content));//切换数据包时，第一次的触发会发现这个内容是上一个数据包的。
+			System.out.println("equal:\n" + ByteArrayUtils.equals(controller.getResponse(), content));
+			System.out.println(this);
+			System.out.println(((InfoPanel) panel).getTable().getInfoTableModel());
+			System.out.println("##################");
+		}
+
+		content = controller.getResponse();
+		//从controller中获取真实的数据包，避免上面提到的，content是上一个数据包的问题。
+		if (content == null || content.length == 0) {
+			return;
+		} else if (ByteArrayUtils.equals(originContent, content)) {
+			return;
+		} else {
+			originContent = content;
+			SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+				@Override
+				protected Void doInBackground() throws Exception {
+					((InfoPanel) panel).getTable().getInfoTableModel().clear();
+					List<String> urls = FindUrlAction.findUrls(originContent);
+
+					for (String url : urls) {
+						InfoEntry aaa = new InfoEntry(url, InfoEntry.Type_URL);
+						((InfoPanel) panel).getTable().getInfoTableModel().addNewInfoEntry(aaa);
+					}
+
+					List<String> emails = EmailUtils.grepEmail(new String(originContent));
+					emails = TextUtils.deduplicate(emails);
+					for (String email : emails) {
+						InfoEntry aaa = new InfoEntry(email, InfoEntry.Type_Email);
+						((InfoPanel) panel).getTable().getInfoTableModel().addNewInfoEntry(aaa);
+					}
+
+					return null;
+				}
+			};
+			worker.execute();
+		}
+	}
 
 
-    public static void main(String[] args) {
-    }
+	@Override
+	public byte[] getMessage() {
+		return originContent;
+	}
+
+	@Override
+	public boolean isModified() {
+		return false;
+	}
+
+	/**
+	 * ctrl+c复制数据逻辑会调用这个函数
+	 */
+	@Override
+	public byte[] getSelectedData() {
+		InfoTable table = (InfoTable) ((InfoPanel) panel).getTable();
+		String content = table.getSelectedContent();
+		return content.getBytes();
+	}
+
+
+	public static void main(String[] args) {
+	}
 }
--- src/messageTab/Info/InfoTableModel.java
+++ src/messageTab/Info/InfoTableModel.java
@@ -10,39 +10,36 @@ import javax.swing.table.AbstractTableModel;
 import burp.BurpExtender;
 
 
-public class InfoTableModel extends AbstractTableModel{
+public class InfoTableModel extends AbstractTableModel {
 	private static final long serialVersionUID = 1L;
 	private List<InfoEntry> infoEntries = new ArrayList<>();
 
 	public static String[] titles = InfoTable.headers;
-	
-	public InfoTableModel(){
-		
+
+	public InfoTableModel() {
+
 	}
 
 	////////////////////// extend AbstractTableModel////////////////////////////////
 
 	@Override
-	public int getColumnCount()
-	{
+	public int getColumnCount() {
 		return titles.length;
 	}
 
 	@Override
-	public Class<?> getColumnClass(int columnIndex)
-	{
+	public Class<?> getColumnClass(int columnIndex) {
 		if (titles[columnIndex].equals("#")) {
 			return Integer.class;//index
-		}else if (titles[columnIndex].equals("Enable")) {
+		} else if (titles[columnIndex].equals("Enable")) {
 			return boolean.class;//enable
-		}else {
+		} else {
 			return String.class;
 		}
 	}
 
 	@Override
-	public int getRowCount()
-	{
+	public int getRowCount() {
 		return infoEntries.size();
 	}
 
@@ -51,7 +48,7 @@ public class InfoTableModel extends AbstractTableModel{
 	public String getColumnName(int columnIndex) {
 		if (columnIndex >= 0 && columnIndex <= titles.length) {
 			return titles[columnIndex];
-		}else {
+		} else {
 			return "";
 		}
 	}
@@ -60,27 +57,25 @@ public class InfoTableModel extends AbstractTableModel{
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
 		return false;
 	}
-	
-	public InfoEntry getEntryAt(int rowIndex)
-	{
+
+	public InfoEntry getEntryAt(int rowIndex) {
 		return infoEntries.get(rowIndex);
 	}
 
 	@Override
-	public Object getValueAt(int rowIndex, int columnIndex)
-	{
+	public Object getValueAt(int rowIndex, int columnIndex) {
 		InfoEntry entry = infoEntries.get(rowIndex);
 		if (titles[columnIndex].equals("#")) {
 			return rowIndex;
-		}else if (titles[columnIndex].equals("Value")) {
+		} else if (titles[columnIndex].equals("Value")) {
 			return entry.getValue();
-		}else if (titles[columnIndex].equals("Type")) {
+		} else if (titles[columnIndex].equals("Type")) {
 			return entry.getType();
-		}else if (titles[columnIndex].equals("Enable")) {
+		} else if (titles[columnIndex].equals("Enable")) {
 			return entry.isEnable();
-		}else if (titles[columnIndex].equals("Comment")) {
+		} else if (titles[columnIndex].equals("Comment")) {
 			return entry.getComment();
-		}else{
+		} else {
 			return "";
 		}
 	}
@@ -96,13 +91,13 @@ public class InfoTableModel extends AbstractTableModel{
 
 		if (titles[columnIndex].equals("#")) {
 
-		}else if (titles[columnIndex].equals("Value")) {
+		} else if (titles[columnIndex].equals("Value")) {
 			entry.setValue((String) value);
-		}else if (titles[columnIndex].equals("Type")) {
+		} else if (titles[columnIndex].equals("Type")) {
 			entry.setType((String) value);
-		}else if (titles[columnIndex].equals("Enable")) {
-			entry.setEnable((boolean)value);
-		}else if (titles[columnIndex].equals("Comment")) {
+		} else if (titles[columnIndex].equals("Enable")) {
+			entry.setEnable((boolean) value);
+		} else if (titles[columnIndex].equals("Comment")) {
 			entry.setComment((String) value);
 		}
 		fireTableCellUpdated(row, columnIndex);
@@ -110,20 +105,20 @@ public class InfoTableModel extends AbstractTableModel{
 
 	//////////////////////extend AbstractTableModel////////////////////////////////
 
-	public void addNewInfoEntry(InfoEntry lineEntry){
+	public void addNewInfoEntry(InfoEntry lineEntry) {
 		synchronized (infoEntries) {
 			infoEntries.add(lineEntry);
 			int row = infoEntries.size();
 			//fireTableRowsInserted(row, row);
 			//need to use row-1 when add setRowSorter to table. why??
 			//https://stackoverflow.com/questions/6165060/after-adding-a-tablerowsorter-adding-values-to-model-cause-java-lang-indexoutofb
-			fireTableRowsInserted(row-1, row-1);
+			fireTableRowsInserted(row - 1, row - 1);
 			//fireTableRowsInserted(row-2, row-2);
 		}
 	}
 
 
-	public void removeInfoEntry(InfoEntry lineEntry){
+	public void removeInfoEntry(InfoEntry lineEntry) {
 		synchronized (infoEntries) {
 			int index = infoEntries.indexOf(lineEntry);
 			if (index != -1) {
@@ -139,27 +134,32 @@ public class InfoTableModel extends AbstractTableModel{
 			//because thread let the delete action not in order, so we must loop in here.
 			//list length and index changed after every remove.the origin index not point to right item any more.
 			Arrays.sort(rows); //升序
-			for (int i=rows.length-1;i>=0 ;i-- ) {//降序删除才能正确删除每个元素
+			for (int i = rows.length - 1; i >= 0; i--) {//降序删除才能正确删除每个元素
 				InfoEntry config = infoEntries.get(rows[i]);
 				infoEntries.remove(rows[i]);
-				stdout1.println("!!! "+config.getValue()+" deleted");
+				stdout1.println("!!! " + config.getValue() + " deleted");
 				this.fireTableRowsDeleted(rows[i], rows[i]);
 			}
 		}
 	}
 
+	public void clear() {
+		synchronized (infoEntries) {
+			infoEntries = new ArrayList<>();
+		}
+	}
 
 	public void updateRows(int[] rows) {
 		synchronized (infoEntries) {
 			//because thread let the delete action not in order, so we must loop in here.
 			//list length and index changed after every remove.the origin index not point to right item any more.
 			Arrays.sort(rows); //升序
-			for (int i=rows.length-1;i>=0 ;i-- ) {//降序删除才能正确删除每个元素
+			for (int i = rows.length - 1; i >= 0; i--) {//降序删除才能正确删除每个元素
 				InfoEntry checked = infoEntries.get(rows[i]);
 				infoEntries.remove(rows[i]);
 				infoEntries.add(rows[i], checked);
 			}
-			this.fireTableRowsUpdated(rows[0], rows[rows.length-1]);
+			this.fireTableRowsUpdated(rows[0], rows[rows.length - 1]);
 		}
 	}
 
